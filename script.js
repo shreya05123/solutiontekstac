2c4b68850b25f3403f9cc6e027c6fe82445189bf
shreya05123 <shreraj05@gmail.com>
2026-09-16 11:33:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,14 @@
-// Global array to store customer objects
-let customers = [];
+function validateProductCode() {
 
-// Function to add customer
-function addCustomer() {
+    let productCode = document.getElementById("productCode").value;
 
-    // Get values from HTML input fields
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let age = document.getElementById("age").value;
-    let height = document.getElementById("height").value;
-    let weight = document.getElementById("weight").value;
+    let pattern = /^#[A-Z]{4}[0-9]{3}$/;
 
-    // Create customer object
-    let customer = {
-        name: name,
-        email: email,
-        age: age,
-        height: height,
-        weight: weight
-    };
-
-    // Add customer object to global array
-    customers.push(customer);
-
-    // Display customers
-    displayCustomers();
-
-    // Clear the form
-    clearForm();
-}
-
-
-// Function to display customers in a table
-function displayCustomers() {
-
-    let output = `
-        <table border="1">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Age</th>
-                <th>Height</th>
-                <th>Weight</th>
-            </tr>
-    `;
-
-    // Iterate through the customer array
-    customers.forEach(function(customer) {
-
-        output += `
-            <tr>
-                <td>${customer.name}</td>
-                <td>${customer.email}</td>
-                <td>${customer.age}</td>
-                <td>${customer.height}</td>
-                <td>${customer.weight}</td>
-            </tr>
-        `;
-
-    });
-
-    output += `</table>`;
-
-    // Display table in HTML
-    document.getElementById("customerTable").innerHTML = output;
-}
-
-
-// Function to clear input fields
-function clearForm() {
-
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("age").value = "";
-    document.getElementById("height").value = "";
-    document.getElementById("weight").value = "";
+    if (pattern.test(productCode)) {
+        document.getElementById("result").innerHTML =
+            "Product code verified successfully";
+    } else {
+        document.getElementById("result").innerHTML =
+            "Product code is not valid";
+    }
 }
